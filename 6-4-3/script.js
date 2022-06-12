b0abb64fba6f1da758e65aead075c6116c7b57f3
Ttungaa <tungalag841@gmail.com>
2022-06-12 13:17:05 +0800

    Өгөгдсөн тоог нэгээр нэмж хасах

---
+++ 6-4-3/script.js
@@ -0,0 +1,15 @@
+var input = document.getElementById("Inp");
+var i = 0;
+var y =10;
+i = y;
+input.value = i;
+function minus(){
+    i--;
+    input.value = i;
+    console.log(i)
+}
+function plus(){
+    i++;
+    input.value = i;
+    console.log(i)
+}
